0bde1fa834a7fe9079997b39f56cff667726b3bb
Sujan Mainali <msujan795@gmail.com>
2023-02-16 21:09:56 +0545

    connecting to a database

---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+mongoose.connect(
+    process.env.conn_str,
+    { 
+  
+    useNewUrlParser: true, 
+    useUnifiedTopology: true ,
+
+    },(err) => {
+    if (err) {
+    console.log(err);
+    } else {
+    console.log("mongodb is connected");
+}});
+
+
+
+
+
+mongoose.connection.once('open',() => { 
+    app.emit('ready'); 
+});
+
+app.on('ready', function() { 
+    app.listen(port, () => {
+    console.log('Server started'); 
+  });
+});
+
