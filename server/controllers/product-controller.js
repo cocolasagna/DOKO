8263734164a58da4190690d327a2fdc50063c1af
Sujan Mainali <msujan795@gmail.com>
2023-02-18 20:30:40 +0545

    prodcuts

--- server/controllers/product-controller.js
+++ server/controllers/product-controller.js
@@ -37,4 +37,40 @@ const getOneproduct = async(req,res)=>{
 
 }
 
-module.exports = {addproduct , getallproduct , getOneproduct}
+const deleteproduct = async (req,res)=>{
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id)
+        if(!product){
+            return res.status(400).send()
+        }
+        res.send(product)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+
+const updateproduct = async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "description", "price", "image"];
+    const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+    }
+    try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    });
+    if (!product) {
+    return res.status(404).send();
+    }
+    res.send(product);
+    } catch (error) {
+    res.status(400).send(error);
+    }
+    }
+
+module.exports = {addproduct , getallproduct , getOneproduct , deleteproduct , updateproduct}
--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -7,7 +7,7 @@ const Productrouter = express.Router()
 Productrouter.post('/addproduct',controls.addproduct);
 Productrouter.get('/getallproduct', controls.getallproduct)
 Productrouter.get('/:id', controls.getOneproduct)
-
-
+Productrouter.delete('/:id', controls.deleteproduct)
+Productrouter.patch('/:id', controls.updateproduct)
 
 module.exports = Productrouter; 
