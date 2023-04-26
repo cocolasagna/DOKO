29d755ab39282784edc4d1eee5af792d5fd89dd7
SahilTuladhar <Sailratna11@gmail.com>
2023-04-26 22:36:09 +0545

    sahil

--- my-app/src/components/ProductList/Productlist.jsx
+++ my-app/src/components/ProductList/Productlist.jsx
@@ -5,19 +5,16 @@ import { Link } from "react-router-dom";
 function Productlist(props) {
   return (
     <div className={classes.listCover}>
-      <h1>Your Products</h1>
+      <h1>Your Product</h1>
       <ul className={classes.list}>
         {props.product.map((p) => (
-     
-        <Productitem
+          <Productitem
             image={p.image}
-            Id = {p._id}
+            Id={p._id}
             productName={p.name}
             price={p.price}
-          /> 
-          
+          />
         ))}
-       
       </ul>
     </div>
   );
