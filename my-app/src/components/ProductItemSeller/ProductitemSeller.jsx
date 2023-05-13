3eccc9dcbac01edf3189643a7c77ec96dc6c88aa
SahilTuladhar <Sailratna11@gmail.com>
2023-05-13 12:35:57 +0545

    productitemseller

--- my-app/src/components/ProductItemSeller/ProductitemSeller.jsx
+++ my-app/src/components/ProductItemSeller/ProductitemSeller.jsx
@@ -1,62 +1,60 @@
-import classes from "./productitem.module.css";
-import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlined";
-import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import classes from "./productitemseller.module.css";
+import UpgradeOutlinedIcon from "@mui/icons-material/UpgradeOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
 function Productitem(props) {
-  const PF = 'http://localhost:5000/images/'
+  const PF = "http://localhost:5000/images/";
 
-  const handleClick = async(e)=>{
-        const id = props.Id
-        e.preventDefault()
-        try {
-          const response = await axios.delete(`http://localhost:5000/seller/product/${id}` , {
-            withCredentials: true
-          })
-          console.log(response)
-          window.location.href = "/seller/dashboard"
-        } catch (error) {
-          console.log(error)
+  const handleClick = async (e) => {
+    const id = props.Id;
+    e.preventDefault();
+    try {
+      const response = await axios.delete(
+        `http://localhost:5000/seller/product/${id}`,
+        {
+          withCredentials: true,
         }
-  }
+      );
+      console.log(response);
+      window.location.href = "/seller/dashboard";
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-  
     <div className={classes.itemCover}>
-        <Link to={`/product/${props.Id}`}>
-           
-      <li>
-        <div className={classes.itemImage}>
-       
-          <img src={PF+props.image} alt="no image" />
-        </div>
-     
-        <div className={classes.itemInfo}>
-          <div className={classes.infoUp}>
-            <h2>{props.productName}</h2>
+      <Link to={`/product/${props.Id}`}>
+        <li>
+          <div className={classes.itemImage}>
+            <img src={PF + props.image} alt="no image" />
           </div>
 
-          <div className={classes.infoDown}>
-            <span>{props.price}</span>
+          <div className={classes.itemInfo}>
+            <div className={classes.infoUp}>
+              <h2>{props.productName}</h2>
+            </div>
 
-            <button className={classes.addBtn} onClick={handleClick}>
-              <ShoppingBasketOutlinedIcon className={classes.icon} />
-            </button>
-          
-            <button className={classes.bargainBtn}>
-            <Link to={`/product/update/${props.Id}`}>
-            <LocalOfferIcon className={classes.icon} />
-            </Link>
-            </button>
-           
+            <div className={classes.infoDown}>
+              <span>{props.price}</span>
+
+              <button className={classes.deleteBtn} onClick={handleClick}>
+                <DeleteOutlineOutlinedIcon className={classes.icon} />
+              </button>
+
+              <button className={classes.updateBtn}>
+                <Link to={`/product/update/${props.Id}`}>
+                  <UpgradeOutlinedIcon className={classes.icon} />
+                </Link>
+              </button>
+            </div>
           </div>
-        </div>
-      </li>
-      </Link> 
+        </li>
+      </Link>
     </div>
-
   );
 }
 
