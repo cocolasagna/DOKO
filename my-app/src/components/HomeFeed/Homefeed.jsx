96db44a925386e094aa6d6bbd7cb3fee3379aebd
KrishnaPathak824 <pathakkrishna824@gmail.com>
2023-05-25 22:07:17 +0545

    Sorting

--- my-app/src/components/HomeFeed/Homefeed.jsx
+++ my-app/src/components/HomeFeed/Homefeed.jsx
@@ -16,9 +16,13 @@ function Homefeed() {
         withCredentials: true,
       })
       .then((res) => {
-        setProduct(res.data.products);
-        
-       
+        /*const sortedProducts = res.data.products.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        setProduct(sortedProducts);*/
+        const sortedProducts = res.data.products.sort((a, b) => 
+        a.price - b.price);
+        setProduct(sortedProducts);
       })
       .catch((err) => {
         console.log(err);
